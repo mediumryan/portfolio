50c2ad80ea16a5a221bfb7940835b108f7e36fa9
Ryan <straightwind0726@gmail.com>
2023-06-11 00:18:53 +0900

    [Update] add project's url_path & project filter btn

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,7 +3,9 @@ import ProjectCards from "./ProjectCards";
 import { projects } from "./../datas/projects";
 
 export default function Project() {
-  const [project] = useState(projects);
+  const [project, setProject] = useState(projects);
+  const filtered1 = projects.filter((a) => a.tag !== "React");
+  const filtered2 = projects.filter((a) => a.tag === "React");
 
   return (
     <div id="projects">
@@ -12,6 +14,38 @@ export default function Project() {
         <hr />
       </h3>
       <p>Here are some of my projects, you may like.</p>
+      <div className="projects-btn-box">
+        <button
+          className="projects-btn"
+          onClick={() => {
+            let copy = [...project];
+            copy = projects;
+            setProject(copy);
+          }}
+        >
+          All <span>{projects.length}</span>
+        </button>
+        <button
+          className="projects-btn"
+          onClick={() => {
+            let copy = [...project];
+            copy = filtered1;
+            setProject(copy);
+          }}
+        >
+          HTML, CSS, JS <span>{filtered1.length}</span>
+        </button>
+        <button
+          className="projects-btn"
+          onClick={() => {
+            let copy = [...project];
+            copy = filtered2;
+            setProject(copy);
+          }}
+        >
+          React <span>{filtered2.length}</span>
+        </button>
+      </div>
       <div className="work-box">
         <div className="work">
           {project.map((item) => {
--- src/components/ProjectCards.js
+++ src/components/ProjectCards.js
@@ -4,7 +4,7 @@ export default function ProjectCards({ item }) {
   return (
     <div className="card">
       <img className="work-img" alt={item.title} src={item.image_path} />
-      <a href="/" target="_blank">
+      <a href={item.url_path} target="blank">
         <div className="work-content">{item.title}</div>
       </a>
     </div>
