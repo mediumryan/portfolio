031ad6414d27aadeb6efdcfebb40b24eb671ad66
Ryan <straightwind0726@gmail.com>
2023-09-03 23:55:02 +0900

    [Update] Need to Grid Box styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,67 @@
 import { useRecoilState } from 'recoil';
 import './css/index.css';
 import { projects } from './Data/atom';
+import { styled } from 'styled-components';
+import Greeting from './Components/Greeting';
+import Intro from './Components/Intro';
+import Skills from './Components/Skills';
+import Project from './Components/Project';
+import Contact from './Components/Contact';
 // import components
+
+// style-components
+const MainWrapper = styled.div`
+    background-color: var(--bg-100);
+    color: var(--text-100);
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    padding: 24px 48px;
+`;
+
+const Grid = styled.div`
+    background-color: red;
+    height: 100%;
+    margin-top: 48px;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: repeat(2, 1fr);
+    grid-column-gap: 0px;
+    grid-row-gap: 0px;
+    &:first-child {
+        grid-area: 1 / 3 / 1 / 2;
+        background-color: green;
+    }
+    /* &:nth-child(2) {
+        grid-area: 1 / 2 / 2 / 4;
+        background-color: blue;
+    }
+    &:nth-child(3) {
+        grid-area: 2 / 2 / 3 / 3;
+        background-color: white;
+    }
+    &:last-child {
+        grid-area: 2 / 3 / 3 / 4;
+        background-color: black;
+    } */
+`;
 
 function App() {
     const [project, setProject] = useRecoilState(projects);
 
     console.log(project);
 
-    return <div>hi</div>;
+    return (
+        <MainWrapper>
+            <Greeting />
+            <Grid>
+                <Intro />
+                <Skills />
+                <Project />
+                <Contact />
+            </Grid>
+        </MainWrapper>
+    );
 }
 
 export default App;
---
+++ src/Components/Greeting.jsx
@@ -0,0 +1,39 @@
+import { motion } from 'framer-motion';
+import { styled } from 'styled-components';
+// styled-components
+const GreetingWrapper = styled(motion.div)`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    color: var(--accent-100);
+    font-style: italic;
+    font-size: 36px;
+    font-weight: 700;
+    border-bottom: 2px solid var(--accent-100);
+`;
+
+const greeting_wrapper_variants = {
+    initial: {
+        y: 0,
+        opacity: 0,
+    },
+    animate: {
+        y: 20,
+        opacity: 1,
+        transition: {
+            duration: 3,
+        },
+    },
+};
+
+export default function Greeting() {
+    return (
+        <GreetingWrapper
+            variants={greeting_wrapper_variants}
+            initial="initial"
+            animate="animate"
+        >
+            Hi, I'm Ryan. Welcome to my Portfolio
+        </GreetingWrapper>
+    );
+}
