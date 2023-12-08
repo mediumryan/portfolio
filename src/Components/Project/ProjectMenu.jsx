9a9fb448dbe70245d48d6ed8009c3bbdbcf4b3be
Ryan <straightwind0726@gmail.com>
2023-12-08 13:59:56 +0900

    [Update] Fixed an error on ProjectMenu

--- src/Components/Project/ProjectMenu.jsx
+++ src/Components/Project/ProjectMenu.jsx
@@ -2,59 +2,59 @@ import React from 'react';
 import { useRecoilState, useSetRecoilState } from 'recoil';
 import { styled } from 'styled-components';
 import { nowTypeState, projectTypeState } from '../../Data/atom';
-import { AnimatePresence } from 'framer-motion';
 
 const ProjectMenuWrapper = styled.div`
     display: flex;
     align-items: center;
     width: 100%;
-    margin-bottom: 2rem;
+    margin-bottom: 3rem;
     padding: 0.25rem;
     border: 0.5px solid var(--black-200);
     border-radius: 40px;
     button {
         width: 33%;
-        color: var(--white-200);
         padding: 0.5rem 1rem;
         font-size: 1rem;
         font-weight: 500;
         border-radius: 20px;
         margin: 0 0.25rem;
+        transition: 300ms all;
     }
 `;
 
 export default function ProjectMenu() {
     const [type, setType] = useRecoilState(projectTypeState);
-    const [nowType, setNowType] = useRecoilState(nowTypeState);
+    const setNowType = useSetRecoilState(nowTypeState);
 
     const handleMenu = (item, index) => {
-        const copy = [...type];
-        const updatedType = copy.map((type) => ({
-            ...type,
-            state: false,
-        }));
+        const updatedType = type.map((t) => {
+            return { ...t, state: false };
+        });
         updatedType[index].state = true;
         setType(updatedType);
         setNowType(item.type);
     };
 
+    console.log(type);
+
     return (
         <ProjectMenuWrapper>
             {type.map((item, index) => {
                 return (
-                    <AnimatePresence key={item.id}>
-                        <button
-                            onClick={() => {
-                                handleMenu(item, index);
-                            }}
-                            style={{
-                                backgroundColor:
-                                    item.type === nowType ? '#00619a' : 'none',
-                            }}
-                        >
-                            {item.value}
-                        </button>
-                    </AnimatePresence>
+                    <button
+                        key={item.id}
+                        style={{
+                            backgroundColor: item.state
+                                ? '#00619a'
+                                : 'transparent',
+                            color: item.state ? '#fff' : '#000',
+                        }}
+                        onClick={() => {
+                            handleMenu(item, index);
+                        }}
+                    >
+                        {item.value}
+                    </button>
                 );
             })}
         </ProjectMenuWrapper>
--- src/Pages/Project.jsx
+++ src/Pages/Project.jsx
@@ -8,6 +8,7 @@ const ProjectInner = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: center;
+    padding: 0 5rem;
 `;
 
 const ProjectTitle = styled.h2`
